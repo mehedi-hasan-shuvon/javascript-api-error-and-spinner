b6641aebc58cbc602da88b83c8e1e1177e348e11
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-27 02:55:07 +0600

    practise api

--- api.js
+++ api.js
@@ -1,7 +1,36 @@
 const loadSingleUser = () => {
     fetch('https://randomuser.me/api/')
         .then(res => res.json())
-        .then(data => console.log(data.results[0].gender))
+        .then(data => displaySingleUser(data))
+}
+const loadMeals = (serachText) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${serachText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
 }
 
-loadSingleUser();
+// loadSingleUser();
+loadMeals('fish');
+
+const displaySingleUser = (user) => {
+    console.log(user.results[0].gender)
+}
+
+const displayMeals = (meals) => {
+    const container = document.getElementById('meals');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h1>${meal.strMeal}</h1>
+        <button onclick="loadMealDetails('${meal.strMeal}')">click me</button>
+        `
+        container.appendChild(div);
+
+    });
+}
+
+const loadMealDetails = (mealName) => {
+    console.log(mealName);
+}
