2f01adac4a6f1a61740040ac8315e1006dce8eb9
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-27 03:21:32 +0600

    added spinner

--- api.js
+++ api.js
@@ -19,18 +19,44 @@ const displaySingleUser = (user) => {
 
 const displayMeals = (meals) => {
     const container = document.getElementById('meals');
-    meals.forEach(meal => {
+    container.textContent = '';
+
+    meals?.forEach(meal => {
         console.log(meal);
         const div = document.createElement('div');
         div.innerHTML = `
         <h1>${meal.strMeal}</h1>
+        <p>${meal.strIngredient18 ? meal.strIngredient18 : 'no'}</p>
         <button onclick="loadMealDetails('${meal.strMeal}')">click me</button>
         `
         container.appendChild(div);
 
     });
+    toggleSipnner('none');
+    toggleSearchResults('block');
 }
 
 const loadMealDetails = (mealName) => {
     console.log(mealName);
+}
+
+const toggleSipnner = (displayStyle) => {
+    document.getElementById('spinner').style.display = displayStyle;
+
+}
+
+const toggleSearchResults = (displayStyle) => {
+    document.getElementById('meals').style.display = displayStyle;
+
+}
+
+
+const searchMeal = () => {
+    const searchText = document.getElementById('serach-field').value;
+
+    // display spinner  
+    toggleSipnner('block');
+    toggleSearchResults('none');
+    loadMeals(searchText);
+    document.getElementById('serach-field').value = '';
 }
